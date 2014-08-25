b3a460a88524aedf2abd50cca12084a6a0c2198b
Nicolas <nthomas@robotikdose.com>
2014-08-25 09:29:13 +0200

    25080930

---
+++ platform.cmd
@@ -0,0 +1,10 @@
+echo "----------------"
+echo "removing android"
+echo "----------------"
+cordova platform rm android
+echo "----------------"
+echo "creating android"
+echo "----------------"
+cordova platform add android
+echo "----------------"
+cordova platform ls
