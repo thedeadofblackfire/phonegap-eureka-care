9425d573df978202f4872acb216cc49132bf14b0
Nicolas <nthomas@robotikdose.com>
2014-08-26 11:56:45 +0200

    26081156

--- res.cmd
+++ res.cmd
@@ -2,4 +2,4 @@ echo "----------------"
 echo "Sync res raw"
 echo "----------------"
 mkdir platforms/android/res/raw
-cp res/raw/* platforms/android/res/raw/
+cp www/res/raw/* platforms/android/res/raw/
