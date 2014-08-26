d360e07a59bda32ffb4769d8a3e3ae193b6e5d1f
Nicolas <nthomas@robotikdose.com>
2014-08-26 12:28:35 +0200

    Tue Aug 26 10:28:36 GMT 2014

---
+++ hooks/after_prepare/remove-dev-files.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#rm -rfv ./platforms/ios/www/app ./platforms/ios/www/bower_components ./platform/ios/www/spec ./platforms/ios/www/less
+#rm -rfv ./platforms/android/www/app ./platforms/android/www/bower_comonents ./platform/android/www/spec ./platforms/android/www/less
+rm -rfv ./platforms/android/assets/www/res
