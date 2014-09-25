67372ba8dd70f94320c338f7e3e6e1026947524c
Nicolas <nthomas@robotikdose.com>
2014-09-25 14:30:34 +0200

    Thu Sep 25 12:30:35 GMT 2014

---
+++ platform_ios.sh
@@ -0,0 +1,10 @@
+echo "----------------"
+echo "removing ios"
+echo "----------------"
+cordova platform rm ios
+echo "----------------"
+echo "creating ios"
+echo "----------------"
+cordova platform add ios
+echo "----------------"
+cordova platform ls
